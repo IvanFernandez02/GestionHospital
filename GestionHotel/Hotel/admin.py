25ad7caf2480402917f5658304e8d159842cac5c
Ivan Fernandez <ivan.fernandez@unl.edu.ec>
2024-06-03 19:47:28 -0500

    Codigo de Clases en Models

--- GestionHotel/Hotel/admin.py
+++ GestionHotel/Hotel/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from Hotel.models import *
+
 # Register your models here.
+admin.site.register(Persona)
+admin.site.register(Cliente)
+admin.site.register(Empleado)
+admin.site.register(TipoHabitacion)
+admin.site.register(Habitacion)
+admin.site.register(EstadoReserva)
+admin.site.register(Reservacion)
+admin.site.register(Factura)
+admin.site.register(Transaccion)
+admin.site.register(Pago)
+admin.site.register(PagoTarjeta)
+admin.site.register(PagoEfectivo)
+admin.site.register(CheckIn)
+admin.site.register(CheckOut)
+
+
+
